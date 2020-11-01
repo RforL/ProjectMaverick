cf81aaea494d5bc17d11589f7134a37d979eab2e
zR31GN <57340437+zR31GN@users.noreply.github.com>
2020-11-02 09:51:36 +1300

    awd

---
+++ Assessment2/Transition/Transition5.gd
@@ -0,0 +1,30 @@
+extends RichTextLabel
+
+var dialog = ["After battling through the harsh terrain of Texas", "He finally makes his way to the Lousianan", ""]
+var page = 0
+var num = 0
+
+func _ready():
+	set_bbcode(dialog[page])
+	set_visible_characters(0)
+	set_process_input(true)
+	
+func _input(event):
+	if event is InputEventMouseButton:
+		if get_visible_characters() > get_total_character_count():
+			if page < dialog.size()-1:
+				page += 1
+				set_bbcode(dialog[page])
+				set_visible_characters(0)
+				num += 1
+				print(num)
+				
+			if (num == 2):
+				get_tree().change_scene("res://Levels/Level6/Level6.tscn")
+	
+
+
+func _on_Timer_timeout():
+	print("print")
+	set_visible_characters(get_visible_characters()+1)
+
