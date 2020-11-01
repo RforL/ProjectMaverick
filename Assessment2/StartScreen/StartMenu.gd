0de81e35a1b922f7478321a83ef15ef0ee2d9528
zR31GN <57340437+zR31GN@users.noreply.github.com>
2020-11-02 11:09:12 +1300

    quit screen

--- Assessment2/StartScreen/StartMenu.gd
+++ Assessment2/StartScreen/StartMenu.gd
@@ -9,3 +9,10 @@ func _ready():
 func _on_Button_pressed():
 	get_tree().change_scene("res://Dialogue/Dialogue1.tscn")
 	pass # Replace with function body.
+	
+	
+
+
+func _on_Button3_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
